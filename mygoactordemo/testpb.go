cd26e8a6f476b1535590541b1b7a876f7ef486a0
jerryduren <jerryduren@163.com>
2018-02-10 11:11:02 +0800

    modified

---
+++ mygoactordemo/testpb.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"github.com/golang/protobuf/proto"
+	"github.com/jerryduren/myactorproject/myinclude"
+)
+
+func main() {
+	test := &myinclude.Test {
+		Label: proto.String("hello"),
+		Type:  proto.Int32(17),
+		Reps:  []int64{1, 2, 3},
+		Optionalgroup: &myinclude.Test_OptionalGroup {
+			RequiredField: proto.String("good bye"),
+		},
+	}
+	data, err := proto.Marshal(test)
+	if err != nil {
+		log.Fatal("marshaling error: ", err)
+	}
+	newTest := &myinclude.Test{}
+	err = proto.Unmarshal(data, newTest)
+	if err != nil {
+		log.Fatal("unmarshaling error: ", err)
+	}
+	// Now test and newTest contain the same data.
+	if test.GetLabel() != newTest.GetLabel() {
+		log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
+	}
+	// etc.
+}
