10a1c4a457dff1d357512d3ccf8d37e9d9b2659d
jerryduren <jerryduren@163.com>
2018-02-15 09:54:56 +0800

    modified

--- mygoactordemo/ActorHelloWorld.go
+++ mygoactordemo/ActorHelloWorld.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/AsynkronIT/protoactor-go/actor"
 	"time"
+	"os"
 )
 
 type HelloActor struct {
@@ -14,12 +15,20 @@ type HelloActor struct {
 func (this *HelloActor)Receive(context actor.Context){
 	switch msg:=  context.Message  ().(type){
 	case myinclude.HelloMessage:
-		fmt.Println(msg.Say)
+		fmt.Println(msg.Who,"say",msg.Say)
 	}
 }
 func main() {
 	pid:=actor.Spawn(actor.FromInstance(&HelloActor{}))
-	pid.Tell(myinclude.HelloMessage{Say:"Hello, I am Jerry Du!"})
+	if len(os.Args)==2{
+		pid.Tell(myinclude.HelloMessage{Who:os.Args[1]})
+	}else if len(os.Args)==3{
+		pid.Tell(myinclude.HelloMessage{Who:os.Args[1],Say:os.Args[2]})
+	}else if len(os.Args)==1{
+		pid.Tell(myinclude.HelloMessage{Who:"",Say:"Hello, I am Jerry Du!"})
+		}else {
+			fmt.Println("Usge: actorhelloword <who> <say>")
+	}
 
 	time.Sleep(time.Second)
 }
